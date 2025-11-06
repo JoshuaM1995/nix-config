31dd419ef4e682bf9b7c31ead27f845fbe198e44
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-11-06 10:42:03 -0700

    Remove Spotify indicator and related scripts from Sketchybar configuration
    - Deleted the Spotify indicator item from the Sketchybar configuration to streamline the setup.
    - Removed associated scripts for Spotify playback control and indicator functionality, simplifying the overall configuration and reducing resource usage.

--- configs/sketchybar/plugins/spotify_next_click.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-osascript -e 'tell application "Spotify" to next track'
-sketchybar -m --update spotify_indicator
-
--- configs/sketchybar/plugins/spotify_play_click.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-osascript -e 'tell application "Spotify" to playpause'
-sketchybar -m --update spotify_indicator
-
