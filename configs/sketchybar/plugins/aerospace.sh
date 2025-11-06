4cc5e983e9faab603dda707b549d6c87bae340d1
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-11-06 14:30:42 -0700

    Update aerospace.sh to modify label colors based on workspace focus
    - Added conditional label colors for focused and unfocused workspaces to enhance visual distinction.
    - This change aims to improve user experience by providing clearer feedback on the active workspace.

--- configs/sketchybar/plugins/aerospace.sh
+++ configs/sketchybar/plugins/aerospace.sh
@@ -2,8 +2,10 @@
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set $NAME background.drawing=on \
+                              label.color=0xff2BEEE3 \
                               label.font="SF Pro:Bold:13.0"
 else
     sketchybar --set $NAME background.drawing=off \
+                              label.color=0xffffffff \
                               label.font="SF Pro:Regular:13.0"
 fi
